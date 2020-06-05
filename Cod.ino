047fb2ea3ea9607bdf1dc45836e1660aa793a214
Robert-Adrian <57811734+Robert-Adrian@users.noreply.github.com>
2020-06-05 12:30:53 +0300

    Update Cod.ino

--- Cod.ino
+++ Cod.ino
@@ -1,274 +1,326 @@
-void setup() {
-  // put your setup code here, to run once:
-  Timer0();
-  Timer1();
-  PiniMotoare();
-  PiniPWM();
-  PiniSonar();
-  PiniSenzorLinie();
-}
-////////////////////////////////////////////////////////////////////////
-//Variabile globale
+#define VN 170
+#define KP 15
 
-int overflow=0;
-double distance;
-long count;
-int viteza_nominala=200;
-int kp=20;
+int i=0;
+int n = 0;                //contor overflow sonar
+int distanta=0;
+int oprire=0;
 
-////////////////////////////////////////////////////////////////////////
-//TIMER0 pentru utilizarea motoarelor
-
-void Timer0(){
-  cli();
-  TCCR0A=0;
-  TCCR0B=0;
-  TCCR0A|=(1<<WGM01)|(1<<WGM00)|(1<<COM0B1)|(1<<COM0A1); //setare fast pwm
-  TCCR0B|=(1<<CS00); //fara prescaler
-  sei();  
-}
-////////////////////////////////////////////////////////////////////////
-//TIMER1 pentru calculul distantei de la sonar
-
-void Timer1(){
-  cli();
-  TCCR1A=0;
-  TIMSK1|=(1<<TOIE1); //activeaza intreruperea overflow
+void timer1(){
+  cli();                    //dezactiveaza toate intreruperile
+  DDRD|=0x80;               //pin 7 output trig
+  DDRB&=~0x01;              //pin 8 input echo  
+  TCCR1A = 0;                             //curatare registrii
+  TCCR1B = 0;                 
+  TCCR1B |= (1 << CS10);                    //no prescaling
   sei();
 }
-////////////////////////////////////////////////////////////////////////
-//Intreruperea de tip overflow
 
-ISR(TIMER1_OVF_vect){
-  overflow++;  /* Increment Timer Overflow count */
+void initializare_CAN()
+{
+  ADCSRA |= ((1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0));//factor de divizare 128
+  ADMUX |= (1 << REFS0);//setare tensiune de referinta
+  ADCSRA |= (1 << ADEN);//Activare convertor
+  ADCSRA |= (1 << ADSC);//start conversie CAN
 }
 
-////////////////////////////////////////////////////////////////////////
+void setare_color(){
+  // pini 2 3 4 (pd2 pd3 pd4) pentru culoare rgb
+  //2- red 
+  //3-green
+  //4 -blue
+  DDRD|=0x1C; //setare pini output RGB
+  // analog 5 (A5) PC5 pin input fotorezistor
+  DDRC&=~0x20;
+}
 
-void Timer2(){
+void setare_PID(){
+  DDRC &= ~0X1F; // PINII A0-A4 INPUT BANDA LEDURI
+}
+
+void pwm() {
+ cli();    //dezactiveaza toate intreruperile
+  DDRB|=0x1E;            //pin 9,10,11,12 iesire pentru in1,2,3,4 directie motor
+  DDRD|=0X60;            //pin 5,6 pwm output 
+  TCCR0A=0;
+  TCCR0B=0;                                        //curatare registrii
+  TCCR0A |= (1 <<WGM01) | (1<<WGM00)|(1<<COM0B1) |(1<<COM0A1);  //setare fast pwm
+  TCCR0B|=(1<<CS00);                               //selectare no prescaling
+ // OCR0B=114;
+  sei();
+}
+
+void timer2(){
   cli();
   TCCR2A=0;
   TCCR2B=0;
-  TCCR2B|=(0 << CS22)|(1 << CS21)|(0 << CS20); //prescaler 8
+  TCCR2B |= (1<< CS20) | (1<<CS21)|(1<<CS22);     //PRESCALER 1024
+  TCCR2A|=(1<<WGM21);       //MOD CTC
+  TIMSK2 |=(1<<OCIE2A);         //ACTIVARE INTRERUPERE LA COMPARARE A;
+  OCR2A=158;          //INTRERUPERE LA 10MS
   sei();
 }
-////////////////////////////////////////////////////////////////////////
-//Setarea serialului pentru transmitere date + functie de transmitere seriala
 
-void USART_Transmit(unsigned char data)
-{
-   while(!( UCSR0A & (1<<UDRE0))); 
-   UDR0=data;
+
+ISR(TIMER2_COMPA_vect){
+  i++;
 }
-
-void SetareSerial(){
-   //Setare serial
-  UCSR0C=(1<<UCSZ00)|(1<<UCSZ01); //Numarul bitilor de date
-  UCSR0B=(1<<RXEN0)|(1<<TXEN0); //Activez receptia si transmisia
-  UCSR0A=(1<<UDRE0);
-  UBRR0=103; //Baud rate 9600
-  }
-
-////////////////////////////////////////////////////////////////////////
-//Functii delay
-
-void Delay3s(){
-  int durata=0;
-  TCNT2=0;
-  while(durata<=23529)//16MHz/8=2MHz; 3S/(0.5us*255)=23529
-  { 
-    if(TCNT2==255)
-    {
-      TCNT2=0;
-      durata++;
-    }
-    TCNT2++;
-  }
-}
-
-void Delay1s(){
-  int durata=0;
-  TCNT2=0;
-  while(durata<=7843)//16MHz/8=2MHz; 1S/(0.5us*255)=7843
-  { 
-    if(TCNT2==255)
-    {
-      TCNT2=0;
-      durata++;
-    }
-    TCNT2++;
-  }
-}
-
-void Delay10ms(){
-  int durata=0;
-  TCNT2=0;
-  while(durata<=392)//16MHz/8=2MHz; 10ms/(0.5us*255)=78
-  { 
-    if(TCNT2==255)
-  {
-    TCNT2=0;
-    durata++;
-    }
-    TCNT2++;
-  }
-}
-
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-//Functie pentru trimiterea distantei pe serial
-void TrimiteDist(int var)
-{
-  char buf[50];
-  memset(buf,0,50);
-  sprintf(buf,"Distanta este: %d cm\n", var);
-  for(int i=0;i<strlen(buf);i++)
-    USART_Transmit(buf[i]);
-}
-
-////////////////////////////////////////////////////////////////////////
-//Setare pini
-
-void PiniPWM(){ //D5,D6 -> ENB,ENA
-  DDRD|=0x60;  
-}
-
-////////////////////////////////////////////////////////////////////////
-
-void PiniMotoare(){ //IN1,IN2,IN3,IN4 -> B1,B2,B3,B4 | ENA,ENB -> D3,D4
-  DDRB|=0x1E;
-}
-
-////////////////////////////////////////////////////////////////////////
-
-void PiniSenzorLinie(){//A0,A1,A2,A3,A4
-  DDRC|=0x1F;
-}
-
-////////////////////////////////////////////////////////////////////////
-
-void PiniSonar(){ //D7,B0
-  DDRD|=0x80;
-  DDRB|=0x01;
-}
-
-////////////////////////////////////////////////////////////////////////
-int SenzorLinie(){  
-  int eroare=0;
-  if(PINC&0X1F==0x01)
-  { Dreapta();
-    eroare=-2;
-  }
-  else if(PINC&0X1F==0x02)
-  {
-    Fata();
-    eroare=-1;
-  }
-  else if(PINC&0X1F==0x04)
-  {
-    Fata();
-    eroare=0;
-  }
-  else if(PINC&0X1F==0x08)
-  {
-    Fata();
-    eroare=1;
-  }
-  else if(PINC&0X1F==0x10)
-  {
-    Stanga();
-    eroare=2;
-  }
-  return eroare;
-}
-////////////////////////////////////////////////////////////////////////
-
-void CalculPID()
-{
-  int eroare=0;
-  int v1,v2;
-  eroare=SenzorLinie();
-  v1=viteza_nominala-kp*eroare;
-  v2=viteza_nominala+kp*eroare;
-}
-
-////////////////////////////////////////////////////////////////////////
-//Functii pentru motoare
 
 void fata(){
-  OCR0A=150;
-  OCR0B=150;
-  PORTB&=~0x1E;
-  PORTB|=0x14;
+  PORTB &=~0X1E;
+  PORTB |=0X0A;
+  OCR0A=200;
+  OCR0B=200;  
 }
 
-/*void Stop(){
-  OCR0A=0;
-  OCR0B=0;
-  PORTB=&~0x1E;
-  PORTD=&~0x18;
-}*/
-
 void spate(){
-  OCR0A=120;
-  OCR0B=120;
-  PORTB&=~0x1E;
-  PORTB|=0x0A;
+  PORTB &=~0X1E;
+  PORTB |=0X14;
+  OCR0A=200;
+  OCR0B=200;
 }
 
 void stanga(){
-  OCR0A=10;
-  OCR0B=120;
-  PORTB&=~0x1E;
-  PORTB|=0x10;
+  PORTB &=~0X1E;
+  PORTB |=0X12;
+  OCR0A=200;
+  OCR0B=200;
 }
 
 void dreapta(){
-  OCR0A=120;
-  OCR0B=10;
-  PORTB&=~0x1E;
-  PORTB|=0x04;
+  PORTB &=~0X1E;
+  PORTB |=0X0C;
+  OCR0A=200;
+  OCR0B=200;
 }
-////////////////////////////////////////////////////////////////////////
 
-void loop() {
+uint16_t citire_CAN()
+{
+  uint32_t Ncan=0;
+  ADMUX |= 0x20;// citire de la pinul A5
+  ADCSRA |= (1 << ADSC);//start conversie
+  while(ADCSRA & (1 << ADSC));//urmarim si cand devine 0 => sfarsitul conversiei
+  Ncan=ADC;
+  return Ncan;
+}
 
-while(1){
-  PORTD&=~0x80; //Trigger pe D7
-  Delay10ms();
-  PORTD|=0x80;
-  Delay10ms();
-  PORTD&=~0x80;
+int track_sensor() {
+  int eroare=0;
+  if((PINC & 0x1B)==0){
+    oprire =1;
+  }
+  if((PINC & 0x08)==0)       //S2(st->dr)
+    eroare=1;
+  else if((PINC & 0x02)==0)   //S4
+          eroare=-1;
+  else if((PINC & 0x10)==0)   //S1
+          eroare=2;
+  else if((PINC & 0x01)==0)   //S5
+          eroare=-2;
+  else if((PINC & 0x04)==0)   //S3
+          eroare=0;
+          
+  return eroare;
+}
+
+void PID(){
+  int e=track_sensor();
+  if((PINC & 0x10)==0){  // senzor 5 activ viraj la stanga
+     PORTB &=~0X1E;
+     PORTB |=0X0A;
+    OCR0A=VN + e*KP;
+    OCR0B=VN - e*KP;
+  }
+  if((PINC & 0x08)==0) {  // senzor 4 activ viraj la stanga
+      PORTB &=~0X1E;
+      PORTB |=0X0A;
+    OCR0A=VN + e*KP;
+    OCR0B=VN - e*KP;
+  }
+  if((PINC & 0x04)==0) {   // senzor 3 activ in fata
+    OCR0A=VN + e*KP;
+    OCR0B=VN - e*KP;
+  }
+  if((PINC & 0x02)==0) {  // senzor 2 activ viraj la dreapta
+     PORTB &=~0X1E;
+     PORTB |=0X0A;
+    OCR0A=VN + e*KP;
+    OCR0B=VN - e*KP;
+  }
+  if((PINC & 0x01)==0) {  // senzor 1 activ viraj la dreapta
+     PORTB &=~0X1E;
+     PORTB |=0X0A;
+    OCR0A=VN + e*KP;
+    OCR0B=VN - e*KP;
+  }
+  if((PINC & 0x1B)==0){
+    PORTB &=~0X1E;
+  }
+}
+
+int culoare(){
+  int cul_r=0,cul_y=0,cul_b=0;
+  int w=0;
   
-  TCNT1=0;
-  TCCR1B=(1<<ICES1)|(1<<CS10);
-  TIFR1=1<<ICF1;
-  TIFR1=1<<TOV1;
+  PORTD|=0X04;
+  delay(10000);
   
-  while ((TIFR1 & (1 << ICF1)) == 1);// Wait for rising edge 
-    {
-      TCNT1 = 0;  /* Clear Timer counter */
-      TCCR1B = 0x01;  /* Capture on falling edge, No prescaler */
-      TIFR1 = 1<<ICF1; /* Clear ICP flag (Input Capture flag) */
-      TIFR1 = 1<<TOV1; /* Clear Timer Overflow flag */
-      TimerOverflow = 0;/* Clear Timer overflow count */
-    }
+  cul_r=analogRead(A5);
+  // cul_r=citire_CAN();
+  //i=0;
+  //while(i<5);//cat timp i=0 adica asteptam 10 ms
+  
+  PORTD&=~0X04;
+  PORTD|=0X0C;
+  delay(10000);
+  
+  cul_y=analogRead(A5);
+ // cul_y=citire_CAN();
+  //i=0;
+  //while(i<10);//cat timp i=0 adica asteptam 10 ms
+  
+  PORTD&=~0X0C;
+  PORTD|=0X10;
+  delay(10000);
+  cul_r=analogRead(A5);
+  //cul_b=citire_CAN();
+  //i=0;
+  //while(i<10);//cat timp i=0 adica asteptam 10 ms
 
-  while ((TIFR1 & ~(1 << ICF1)) == 0);/* Wait for falling edge */
-    {
-      count = ICR1 + (65535 * overflow); /* Take count */
-      /* 16MHz Timer freq, sound speed =343 m/s */
-      distance = ((double)count / 932.94);
+  
+  if((cul_r>cul_y) && (cul_r > cul_b)){
+    //red
+    w=1;
+  }
+  else
+    if((cul_b>cul_y) && (cul_b > cul_r)){
+    //blue
+    w=2;
     }
-    if(distance<40)
-    {
-      stanga();
-      Delay1s();
-      fata();
-      Delay10ms();
-      dreapta();
-      Delay1s();
-      fata();
+    else{
+      //yellow
+      w=3;
     }
- }
+  
+  PORTD&=~0X10;
+  return w;
+}
+
+void sonar()
+{
+  float timp;
+                
+  TCNT1 = 0;
+  PORTD &= ~0x80;
+  while(TCNT1 <= 160);                             // asteapta 10us   adica 160 tcnt1
+  PORTD |= 0x80;
+  while(TCNT1 <= 320);                            // asteapta 10us   adica 160 tcnt1
+  PORTD &= ~0x80;
+  TCNT1 = 0;
+  
+  while(PINB & 0x01) {
+    if(TCNT1 == 65535) {
+      n++;
+      TCNT1 = 0;
+    }
+  }
+
+  timp = (((n*65535) + TCNT1)*0.0625)/1000000;      //timpul  in secunde 
+  distanta = timp*17150;
+
+  if ((distanta > 1) ){
+    if(distanta<20){ // daca e obstacol
+    //aici se va implementa depasirea obstacolului 
+    //deplasare la stanga
+    //apoi in fata pt un anumit timp
+    //apoi revenire pana se va semnala vectorul de senzori de linie
+    //si apoi se va regla pozitia cu PID
+    // ocolire();
+    }
+    else{//daca nu e obstacol
+      if(oprire == 1){ //daca sa gatat linia
+        int cul ;
+        cul=culoare();
+        if(cul==1){
+           delay(3000);
+           fata();
+           delay(1000);
+           //pana cand vede linie
+        }
+        else if(cul==2){
+          //oprire 
+          dreapta();
+          delay(1000);
+          //inapoi pe traseu
+        }
+        else if(cul==3){
+          fata();
+          delay(1000);
+           //pana cand vede linie
+        }
+      }
+      else { //altfel inseamna ca suntem pe traseu si continuam sa urmarim linia 
+        PID();
+      }
+      
+    }
+    n=0;
+  }
+}
+
+void setup() {
+  DDRD=0x00;
+  DDRB=0x00;
+  pwm();
+  timer2(); 
+  setare_PID();
+  setare_color();
+  initializare_CAN();
+  timer1();
+  //Serial.begin(9600);
+}
+void loop(){
+ // PID();
+  sonar();
+  /*
+  int cul=0 ;
+  cul=culoare();
+        if(cul==1){
+           Serial.println("rosu");
+           //pana cand vede linie
+        }
+        else if(cul==2){
+           Serial.println("galben");
+          //inapoi pe traseu
+        }
+        else if(cul==3){
+           Serial.println("albastru");
+           //pana cand vede linie
+        }
+  */
+  /*
+  int cul_r=0,cul_y=0,cul_b=0;
+  PORTD|=0X04;
+  delay(10000);
+  
+  cul_r=analogRead(A5);
+
+  PORTD&=~0X04;
+  PORTD|=0X0C;
+  delay(10000);
+  
+  cul_y=analogRead(A5);
+
+  PORTD&=~0X0C;
+  PORTD|=0X10;
+  delay(10000);
+  cul_b=analogRead(A5);
+  Serial.println("rosu");
+  Serial.println(cul_r);
+  Serial.println("galben");
+  Serial.println(cul_y);
+  Serial.println("albastru");
+  Serial.println(cul_b);
+  PORTD&=~0X10;
+  */
 }
